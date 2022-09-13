af695cfccb6bba4f06159b1c93f22b3e1bf276b1
Diego Felipe Martins <diego.difm@gmail.com>
2022-09-13 20:51:30 -0300

    Novo projeto Angular

---
+++ Angular_Entra21/src/app/vacinas/vacinas-routing.module.ts
@@ -0,0 +1,13 @@
+import { VacinaListagemComponent } from './vacina-listagem/vacina-listagem.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: VacinaListagemComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class VacinasRoutingModule { }
